67500a85970c21159757afc0e69dc2fd339c6ffb
unknown <dawid.krynski@student.uj.edu.pl>
2025-03-01 23:59:23 +0100

    initial commit

---
+++ frontend/src/components/LoadingIndicator.jsx
@@ -0,0 +1,7 @@
+import '../styles/LoadingIndicator.css'
+
+export default function LoadingIndicator () {
+    return (
+        <div className = "loading-content"><div className="loader"></div></div>
+    )
+}
---
+++ frontend/src/pages/NotFound.jsx
@@ -0,0 +1,8 @@
+function NotFound() {
+  return <div>
+    <h1>404 Not found</h1>
+    <p>The page you are looking for does not seem to exist </p>
+  </div>;
+}
+
+export default NotFound;
